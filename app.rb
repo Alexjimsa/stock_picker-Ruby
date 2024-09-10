d1ee42844db83dd0e9d5ef9f450f5fbb4764b21d
Alexjimsa <alexjimsa10@gmail.com>
2024-09-10 19:24:48 +0200

    Works if time travel was invented

---
+++ app.rb
@@ -0,0 +1,20 @@
+price_per_day = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+
+
+
+
+def stock_picker(stock_prices)
+  best_combination = [0, 1]
+  highest_profit = 0
+  stock_prices.each_with_index do |buy, buy_index|
+    stock_prices.each_with_index do |sell, sell_index|
+      if (sell - buy) > highest_profit
+        best_combination = [buy_index, sell_index]
+        highest_profit = sell - buy
+      end
+    end
+  end
+  return best_combination
+end
+
+p stock_picker(price_per_day)
